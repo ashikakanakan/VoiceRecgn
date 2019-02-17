fa68d3a12277eb6d813137f7881685cc49ec0828
ashika.t <ashika.t@P10-LAPTOP-051>
2019-02-17 02:19:23 -0500

    voice record

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import '../assets/appFiles/app.js'
+declare var initialize: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor() {}
+
   title = 'VoiceRecogn';
+  ngOnInit() {
+    this.initializeVar();
+  }
+
+  public initializeVar() : void {
+    initialize.funcDeclar();
+  }
 }
